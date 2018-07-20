d7faf42d608006bc332d291afcb9d17c74312f98
moaldanabernal <41198342+moaldanabernal@users.noreply.github.com>
2018-07-20 14:27:31 -0500

    Fix code style issues

--- src/make_report.py
+++ src/make_report.py
@@ -1,11 +1,10 @@
-# python make_report.py ../data/processed/avengers_processed.csv ../reports/top_ten_appearances.md
-
 import csv
 import sys
 import operator
-from msds510.util import *
+from msds510.util import Avenger
 
-#1. Read file .csv
+# 1. Read file .csv
+
 
 def read_file(file_name):
     with open(file_name, 'r') as lines:
@@ -15,25 +14,27 @@ def read_file(file_name):
         for keys in range(len(column_names)):
             column_names[keys] = column_names[keys].lower()
             column_names[keys] = column_names[keys].strip('\n').strip('?').rstrip().lstrip()
-            column_names[keys] = column_names[keys].replace('/','_').replace(' ', '_')
+            column_names[keys] = column_names[keys].replace('/', '_').replace(' ', '_')
 
         ten_rows = [Avenger(dict(zip(column_names, row))) for row in records]
         ten_rows.sort(key=operator.attrgetter('appearancesInComics'), reverse=True)
         return ten_rows[0:10]
 
-#2. Create report
+# 2. Create report
+
 
 def create_report(report_name, top_ten_rows):
     with open(report_name, 'w') as write_record:
         for record in range(len(top_ten_rows)):
-            write_record.writelines(("# ",str((record+ 1))+". ", top_ten_rows[record].name, '\n\n'))
+            write_record.writelines(("# ", str((record + 1)) + ". ", top_ten_rows[record].name, '\n\n'))
             write_record.writelines(("* Number of Appearances: ", str(top_ten_rows[record].appearancesInComics), '\n'))
             write_record.writelines(("* Year Joined: ", str(top_ten_rows[record].joinYear), '\n'))
             write_record.writelines(("* Years Since Joining: ", str(top_ten_rows[record].yearsSince), '\n'))
             write_record.writelines(("* URL: ", top_ten_rows[record].assignedURL, '\n\n'))
             write_record.writelines(("## Notes\n\n"+top_ten_rows[record].notesData, '\n\n'))
 
-#Main function
+# Main function
+
 
 if __name__ == "__main__":
     create_report(sys.argv[2], read_file(sys.argv[1]))

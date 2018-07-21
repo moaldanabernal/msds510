41f37645d593fb26ce63bc5c69e6c8922eaeccec
moaldanabernal <41198342+moaldanabernal@users.noreply.github.com>
2018-07-20 20:59:11 -0500

    Add documentation strings

--- src/make_report.py
+++ src/make_report.py
@@ -1,13 +1,23 @@
-# python make_report.py ../data/processed/avengers_processed.csv ../reports/top_ten_appearances.md
+'''
+This module creates a markdown report of the top ten Avengers sorted by the number of appearances.
+The user should type two parameters:
+1. The .cvs file name
+2. A markdown report name to create
+'''
 
 import csv
 import sys
 import operator
-from msds510.util import *
+from msds510.util import Avenger
 
-#1. Read file .csv
+'''1. Read a file .csv'''
+
 
 def read_file(file_name):
+    '''
+    :param get a file .csv:
+    :return: return a list of the top ten Avengers sorted by the number of appearances
+    '''
     with open(file_name, 'r') as lines:
         records = csv.reader(lines)
         column_names = next(records)
@@ -15,25 +25,37 @@ def read_file(file_name):
         for keys in range(len(column_names)):
             column_names[keys] = column_names[keys].lower()
             column_names[keys] = column_names[keys].strip('\n').strip('?').rstrip().lstrip()
-            column_names[keys] = column_names[keys].replace('/','_').replace(' ', '_')
+            column_names[keys] = column_names[keys].replace('/', '_').replace(' ', '_')
 
         ten_rows = [Avenger(dict(zip(column_names, row))) for row in records]
         ten_rows.sort(key=operator.attrgetter('appearancesInComics'), reverse=True)
         return ten_rows[0:10]
 
-#2. Create report
+'''2. Create report'''
+
 
 def create_report(report_name, top_ten_rows):
+    '''
+    :param get a markdown report name to create:
+    :param get a list of the top ten Avengers sorted by the number of appearances
+    :return: not return
+    '''
     with open(report_name, 'w') as write_record:
         for record in range(len(top_ten_rows)):
-            write_record.writelines(("# ",str((record+ 1))+". ", top_ten_rows[record].name, '\n\n'))
+            write_record.writelines(("# ", str((record + 1)) + ". ", top_ten_rows[record].name, '\n\n'))
             write_record.writelines(("* Number of Appearances: ", str(top_ten_rows[record].appearancesInComics), '\n'))
             write_record.writelines(("* Year Joined: ", str(top_ten_rows[record].joinYear), '\n'))
             write_record.writelines(("* Years Since Joining: ", str(top_ten_rows[record].yearsSince), '\n'))
             write_record.writelines(("* URL: ", top_ten_rows[record].assignedURL, '\n\n'))
             write_record.writelines(("## Notes\n\n"+top_ten_rows[record].notesData, '\n\n'))
 
-#Main function
+'''Main function'''
+
 
 if __name__ == "__main__":
+    '''
+    call main function with tow input parameters:
+    1. a file .csv
+    2. markdown report name to create
+    '''
     create_report(sys.argv[2], read_file(sys.argv[1]))
--- src/process_csv.py
+++ src/process_csv.py
@@ -1,28 +1,44 @@
-# python process_csv.py ../data/raw/avengers.csv ../data/processed/avengers_processed.csv
+'''
+This module transforms a cvs original file and convert it to utf8
+Then, the file names are improved into the utf8 new file
+'''
 
 import sys
 import csv
 
-# UTF-8 encoded CSV
+
+'''UTF-8 encoded CSV'''
+
 
 with open(sys.argv[1], 'rb') as file:
-  my_oldfile = file.read()
+    '''
+    read the cvs original file and convert it to utf8 
+    '''
+    my_oldfile = file.read()
+    file_iso = my_oldfile.decode('iso-8859-1')
+    file_utf8 = file_iso.encode('utf8')
 
-file_iso = my_oldfile.decode('iso-8859-1')
-file_utf8 = file_iso.encode('utf8')
 
 with open(sys.argv[2], 'wb') as emptyfile:
-  emptyfile.write(file_utf8)
+    '''
+    write to disk new utf8 file
+    '''
+    emptyfile.write(file_utf8)
 
-# Friendly headers
+
+'''Friendly headers'''
+
 
 with open(sys.argv[2], 'r') as in_file:
+    '''
+    read utf8 file and extract first line, which is the original field names, in order to improve them
+    '''
     in_line = csv.DictReader(in_file)
 
     fieldnames = []
     for column in in_line.fieldnames:
         new_header = column.lower()
-        new_header = new_header.replace('/', '_').replace(' ','_')
+        new_header = new_header.replace('/', '_').replace(' ', '_')
         new_header = new_header.strip('\n').strip('?')
         fieldnames.append(new_header)
 
@@ -33,6 +49,9 @@ with open(sys.argv[2], 'r') as in_file:
         record.append(row)
 
 with open(sys.argv[2], 'w', newline='') as new_file:
+    '''
+    write in utf8 file the new field names improved
+    '''
     writer = csv.DictWriter(new_file, fieldnames=in_line.fieldnames)
 
     firstRow = {}
@@ -41,4 +60,3 @@ with open(sys.argv[2], 'w', newline='') as new_file:
     writer.writerow(firstRow)
     for row in record:
         writer.writerow(row)
-
